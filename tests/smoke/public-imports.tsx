bba62431a0bf3fb9c1c4f85eef8af3ae89ede3a0
a <a@a.com>
2026-03-25 21:53:29 +0800

    chore: bootstrap standalone mtxuilib repo

---
+++ tests/smoke/public-imports.tsx
@@ -0,0 +1,6 @@
+import { cn, randomUUID } from "mtxuilib/lib/utils";
+import { Button } from "mtxuilib/ui/button";
+import { DebugValue } from "mtxuilib/mt/DebugValue";
+import { ChatContainerRoot } from "mtxuilib/prompt-kit/chat-container";
+
+void [cn, randomUUID, Button, DebugValue, ChatContainerRoot];
